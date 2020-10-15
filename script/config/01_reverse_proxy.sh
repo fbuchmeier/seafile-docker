6624e537e860ba40841651355e64fb6dd7470ca4
Florian Buchmeier <florian.buchmeier@audi.de>
2020-10-15 11:28:34 +0200

    Fixed 'seahub_settings.py' update deleting every line after 'FILE_SERVER_ROOT'.

--- script/config/01_reverse_proxy.sh
+++ script/config/01_reverse_proxy.sh
@@ -7,10 +7,11 @@ if [[ "$REVERSE_PROXY_MODE" =~ ^https?$ ]]; then
     SERVICE_URL="$REVERSE_PROXY_MODE://$SERVER_ADDRESS"
     SEAFHTTP_URL=${SERVICE_URL//[\/]/\\/}"\/seafhttp"
     crudini --set $EXPOSED_ROOT_DIR/conf/ccnet.conf General SERVICE_URL "$SERVICE_URL"
-    sed -i \
-			-e "/^\(FILE_SERVER_ROOT = \).*/{s//\1'$SEAFHTTP_URL'/;:a;n;:ba;q}" \
-		    -e "\$aFILE_SERVER_ROOT = '$SEAFHTTP_URL'" \
-            $EXPOSED_ROOT_DIR/conf/seahub_settings.py
+    # Update FILE_SERVER_ROOT if it exists, otherwise add it to the end of the file
+    # This way we make sure any other changes to this file (e.g. OAUTH config) do not get lost
+    grep -qx "FILE_SERVER_ROOT.*" $EXPOSED_ROOT_DIR/conf/seahub_settings.py \
+      && sed -i "s~FILE_SERVER_ROOT.*~FILE_SERVER_ROOT = '$SEAFHTTP_URL'~" $EXPOSED_ROOT_DIR/conf/seahub_settings.py \
+      || echo "FILE_SERVER_ROOT = '$SEAFHTTP_URL'" >> $EXPOSED_ROOT_DIR/conf/seahub_settings.py
 else
     log_info "Reverse proxy is not configured"
     crudini --set $EXPOSED_ROOT_DIR/conf/ccnet.conf General SERVICE_URL "http://$SERVER_ADDRESS:8000"
